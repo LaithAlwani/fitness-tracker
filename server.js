961ff077a85885df4790663db5bb4e69b2534bd1
Laith Alwani <laithalwani@gmail.com>
2021-03-22 10:22:20 -0400

    changed file structre to MVC and contected all files together

--- server.js
+++ server.js
@@ -8,14 +8,23 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.post("api/workouts", (req,res) => {
+    console.log(req.body);
+    res.json({});
+});
+
+app.get("/api/workouts", (req,res) =>{
+    res.json({});
+})
+
 app.get("/exercise", (req,res) => {
-    res.sendFile(path.join(__dirname +"/public/exercise.html"));
+    res.sendFile(path.join(__dirname +"/public/views/exercise.html"));
 });
 app.get("/stats", (req,res) => {
-    res.sendFile(path.join(__dirname +"/public/stats.html"));
+    res.sendFile(path.join(__dirname +"/public/views/stats.html"));
 });
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname +"/index.html"));
+    res.sendFile(path.join(__dirname +"/public/views/index.html"));
 });
 
 app.listen(PORT, () => {
