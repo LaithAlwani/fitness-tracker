bb0cbf0ac05cb12f9282577e64364824503d8355
Laith Alwani <laithalwani@gmail.com>
2021-03-24 14:08:00 -0400

    creating and continuing workout works, adding exercises work as well

--- server.js
+++ server.js
@@ -12,37 +12,64 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/fintess", 
-    { useNewUrlParser: true },
-    { useUnifiedTopology: true }
-);
-
-app.post("/api/workouts", (req,res) => {
-    console.log(req.body);
-    //create a new wrok out
-    res.json({});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fintess", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.put("/api/workouts/:id", (req,res) => {
-    console.log(req.body);
-    res.json({});
+app.post("/api/workouts", (req, res) => {
+  console.log(req.body);
+  db.Workout.create({})
+    .then((dbWorkout) => {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
 });
 
-app.get("/api/workouts", (req,res) =>{
-    res.json({});
-})
+app.put("/api/workouts/:id", (req, res) => {
+  db.Exercise.create(req.body).then((dbExercise) => {
+      console.log(dbExercise);
+    db.Workout.findByIdAndUpdate(
+      req.params.id,
+      { $push: { exerciseCollection: dbExercise._id } },
+      { new: true }
+    )
+      .then((dbWorkout) => {
+        console.log("chosen workout" + dbWorkout);
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  });
+});
 
-app.get("/exercise", (req,res) => {
-    res.sendFile(path.join(__dirname +"/public/views/exercise.html"));
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout =>{
+        console.log(dbWorkout);
+        res.json(dbWorkout);
+    }).catch(err => {
+        console.log(err);
+        res.json(err);
+    });
 });
-app.get("/stats", (req,res) => {
-    res.sendFile(path.join(__dirname +"/public/views/stats.html"));
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/views/exercise.html"));
 });
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname +"/public/views/index.html"));
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/views/stats.html"));
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/views/index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log("listning to port " + PORT)
-})
+  console.log("listning to port " + PORT);
+});
