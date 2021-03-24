0197bbcb94b0d020fee09c8f75bf67d05fe08d2b
Laith Alwani <laithalwani@gmail.com>
2021-03-24 08:47:05 -0400

    added workout and exercise model

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+
+const db = require("./models");
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -8,7 +12,19 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.post("api/workouts", (req,res) => {
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/fintess", 
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
+);
+
+app.post("/api/workouts", (req,res) => {
+    console.log(req.body);
+    //create a new wrok out
+    res.json({});
+});
+
+app.put("/api/workouts/:id", (req,res) => {
     console.log(req.body);
     res.json({});
 });
@@ -28,5 +44,5 @@ app.get("/", (req,res) => {
 });
 
 app.listen(PORT, () => {
-    console.log("listning to port" +PORT)
+    console.log("listning to port " + PORT)
 })
